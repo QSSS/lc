9a81b8daceae18b206ac79afae8fccf1976f2ef1
QSSS <QSSS@users.noreply.github.com>
2016-08-23 16:48:56 -0700

    [138. Copy List with Random Pointer][Runtime Error]committed by LeetCode Extension

---
+++ 138-Copy-List-with-Random-Pointer/solution.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * function RandomListNode(label) {
+ *     this.label = label;
+ *     this.next = this.random = null;
+ * }
+ */
+
+/**
+ * @param {RandomListNode} head
+ * @return {RandomListNode}
+ */
+var copyRandomList = function(head) {
+    var map = new Map();
+    var copyHead = new RandomListNode(head.label);
+    var p = head;
+    var q = copyHead;
+    map.set(head, copyHead);
+    p = p.next;
+    
+    while (p) {
+        var nodeCopy = new RandomList(p.label);
+        map.set(p, nodeCopy);
+        q.next = nodeCopy;
+        q = q.next;
+        p = p.next;
+        
+    }
+    
+    p = head;
+    q = copyHead;
+    
+    while (p) {
+        q.random = map.get(p.random);
+        q = q.next;
+        p = p.next;
+    }
+    return newHead;
+};
