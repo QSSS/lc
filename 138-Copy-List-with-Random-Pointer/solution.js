7f90942282e0e110f3688c60dfad23629be14957
QSSS <QSSS@users.noreply.github.com>
2016-08-23 17:23:56 -0700

    [138. Copy List with Random Pointer][Accepted]committed by LeetCode Extension

--- 138-Copy-List-with-Random-Pointer/solution.js
+++ 138-Copy-List-with-Random-Pointer/solution.js
@@ -11,28 +11,31 @@
  * @return {RandomListNode}
  */
 var copyRandomList = function(head) {
- 2     var dict = {};
- 3     var originNode = head;
- 4     var newNode = new RandomListNode(0);
- 5     var originHead = head;
- 6     var newHead = newNode;
- 7     while(originNode !== null){
- 8         var temp = new RandomListNode(originNode.label);
- 9            dict[originNode.label] = temp;
-10            newNode.next = temp;
-11 
-12         newNode = newNode.next;
-13         originNode = originNode.next;
-14     }
-15 
-16     originNode = originHead;
-17     newNode = newHead.next;
-18     while(originNode !== null){
-19         if(originNode.random !== null){
-20             newNode.random = dict[originNode.random.label];
-21         }
-22         newNode = newNode.next;
-23         originNode = originNode.next;
-24     }
-25     return newHead.next;    
-26 };
+    var dict = {};
+    var missed = [];
+    var originNode = head;
+    var newNode = new RandomListNode(0);
+    var originHead = head;
+    var newHead = newNode;
+    while(originNode !== null){
+        var temp = new RandomListNode(originNode.label);
+           dict[originNode.label] = temp;
+           newNode.next = temp;
+           newNode = newNode.next;
+
+           if(originNode.random !== null){
+               if(dict[originNode.random.label]){
+                   newNode.random = dict[originNode.random.label];
+               }else{
+                   newNode.random = originNode.random.label;
+                   missed.push(newNode.label);
+               }
+        }
+        originNode = originNode.next;
+    }
+    for(var i = 0; i < missed.length; i++){
+        var curr = dict[missed[i]];
+        curr.random = dict[curr.random];
+    }
+    return newHead.next;    
+};
