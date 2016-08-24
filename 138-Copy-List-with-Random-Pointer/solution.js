882c0924baf3a5a496c9449b12ca6dd1e2e182c4
QSSS <QSSS@users.noreply.github.com>
2016-08-23 17:23:22 -0700

    [138. Copy List with Random Pointer][Runtime Error]committed by LeetCode Extension

--- 138-Copy-List-with-Random-Pointer/solution.js
+++ 138-Copy-List-with-Random-Pointer/solution.js
@@ -11,32 +11,28 @@
  * @return {RandomListNode}
  */
 var copyRandomList = function(head) {
-    if (!head) {
-        return head;
-    }
-    var map = {};
-    var copyHead = new RandomListNode(head.label);
-    var p = head;
-    var q = copyHead;
-    map[head.label] = copyHead;
-    p = p.next;
-
-    while (p !== null) {
-        var nodeCopy = new RandomListNode(p.label);
-        map[p] = nodeCopy;
-        q.next = nodeCopy;
-        q = q.next;
-        p = p.next;
-
-    }
-
-    p = head;
-    q = copyHead;
-
-    while (p !== null) {
-        q.random = map[p];
-        q = q.next;
-        p = p.next;
-    }
-    return copyHead;
-};
+ 2     var dict = {};
+ 3     var originNode = head;
+ 4     var newNode = new RandomListNode(0);
+ 5     var originHead = head;
+ 6     var newHead = newNode;
+ 7     while(originNode !== null){
+ 8         var temp = new RandomListNode(originNode.label);
+ 9            dict[originNode.label] = temp;
+10            newNode.next = temp;
+11 
+12         newNode = newNode.next;
+13         originNode = originNode.next;
+14     }
+15 
+16     originNode = originHead;
+17     newNode = newHead.next;
+18     while(originNode !== null){
+19         if(originNode.random !== null){
+20             newNode.random = dict[originNode.random.label];
+21         }
+22         newNode = newNode.next;
+23         originNode = originNode.next;
+24     }
+25     return newHead.next;    
+26 };
