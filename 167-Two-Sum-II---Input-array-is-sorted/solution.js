cc7f5943f4551596534601f3e299cca5ea685c1c
QSSS <QSSS@users.noreply.github.com>
2016-08-31 13:33:27 -0700

    [167. Two Sum II - Input array is sorted][Accepted]committed by LeetCode Extension

--- 167-Two-Sum-II---Input-array-is-sorted/solution.js
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.js
@@ -4,18 +4,33 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
+    // if (!numbers) {
+    //     return [];
+    // }
+    
+    // var sums = {};
+    // for (var i = 0, len = numbers.length; i < len; i++) {
+        
+    //     if (sums[numbers[i]] !== undefined){
+            
+    //         return [sums[numbers[i]] + 1, i + 1];
+    //     }
+    //     sums[target - numbers[i]] = i;
+    // }
+    // return [];
+    
     if (!numbers) {
         return [];
     }
-    
-    var sums = {};
-    for (var i = 0, len = numbers.length; i < len; i++) {
-        
-        if (sums[numbers[i]] !== undefined){
-            
-            return [sums[numbers[i]] + 1, i + 1];
+    var left = 0, right = numbers.length - 1, sum;
+    while (left < right) {
+        sum = numbers[left] + numbers[right];
+        if (sum == target) return [left + 1, right + 1];
+        if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
-        sums[target - numbers[i]] = i;
     }
     return [];
 };
